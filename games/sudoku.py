ed30fe080fd08e72084d2a70fb904759d9ea3ab0
Amir Levi <tiptipon81@gmail.com>
2021-04-11 18:37:40 +0300

    Added a click feature per cell grid and added a CellRect class

--- games/sudoku.py
+++ games/sudoku.py
@@ -2,15 +2,17 @@ import pygame, sys
 from pygame.locals import *
 
 
-WINDOW_WIDTH = 1024
-WINDOW_HEIGHT = 768
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 900
 SCREEN_ASPECT_RATIO = WINDOW_WIDTH//WINDOW_HEIGHT
 FPS = 30
-WINDOW_MULTIPLIER = 5 
-WINDOW_SIZE = 90 * SCREEN_ASPECT_RATIO
-SQUARE_SIZE = [WINDOW_WIDTH // 3 * SCREEN_ASPECT_RATIO, WINDOW_HEIGHT // 3 * SCREEN_ASPECT_RATIO ] #(WINDOW_SIZE * WINDOW_MULTIPLIER) // 3 // SCREEN_ASPECT_RATIO
-CELL_SIZE = [SQUARE_SIZE[0] // 3 * SCREEN_ASPECT_RATIO, SQUARE_SIZE[1] // 3 * SCREEN_ASPECT_RATIO ]#SQUARE_SIZE // 3 // SCREEN_ASPECT_RATIO
-NUMBER_SIZE = [CELL_SIZE[0] /3,CELL_SIZE[1] /3]
+WINDOW_MULTIPLIER = 5
+SQUARE_MULTIPLIER = 3
+CELL_MUTLTIPLIER = 3
+#WINDOW_SIZE = 90 * SCREEN_ASPECT_RATIO
+SQUARE_SIZE = [WINDOW_WIDTH // SQUARE_MULTIPLIER * SCREEN_ASPECT_RATIO, WINDOW_HEIGHT // SQUARE_MULTIPLIER * SCREEN_ASPECT_RATIO ]
+CELL_SIZE = [SQUARE_SIZE[0] // CELL_MUTLTIPLIER , SQUARE_SIZE[1] // CELL_MUTLTIPLIER  ]#SQUARE_SIZE // 3 // SCREEN_ASPECT_RATIO
+NUMBER_SIZE = [CELL_SIZE[0] /CELL_MUTLTIPLIER,CELL_SIZE[1] /CELL_MUTLTIPLIER]
 
 BOARD_COLOR = (255,255,255)
 GRID_COLOR = {
@@ -19,82 +21,133 @@ GRID_COLOR = {
 }
 
 
-def draw_grid():
-    
-    for x in range(0, WINDOW_WIDTH, CELL_SIZE[0]): # draw vertical lines
-        pygame.draw.line(DISPLAYSURF, GRID_COLOR['LIGHT_GRAY'], (x,0),(x,WINDOW_HEIGHT))
-    for y in range (0, WINDOW_HEIGHT, CELL_SIZE[1]): # draw horizontal lines
-        pygame.draw.line(DISPLAYSURF, GRID_COLOR['LIGHT_GRAY'], (0,y), (WINDOW_WIDTH, y))
+class Grid:
+    def __init__(self, cells):
+        self.cells = cells
+       
+    def draw_grid(self):
+        for x in range(0, WINDOW_WIDTH, CELL_SIZE[0]): # draw vertical lines
+            pygame.draw.line(DISPLAYSURF, GRID_COLOR['LIGHT_GRAY'], (x,0),(x,WINDOW_HEIGHT))
         
+        for y in range (0, WINDOW_HEIGHT, CELL_SIZE[1]): # draw horizontal lines
+            pygame.draw.line(DISPLAYSURF, GRID_COLOR['LIGHT_GRAY'], (0,y), (WINDOW_WIDTH, y))
+        
+
+        ### Draw Major Lines
+        for x in range(0, WINDOW_WIDTH, SQUARE_SIZE[0]): # draw vertical lines
+            pygame.draw.line(DISPLAYSURF, GRID_COLOR['Black'], (x,0),(x,WINDOW_HEIGHT))
+       
+        for y in range (0, WINDOW_HEIGHT, SQUARE_SIZE[1]): # draw horizontal lines
+            pygame.draw.line(DISPLAYSURF, GRID_COLOR['Black'], (0,y), (WINDOW_WIDTH, y))
     
+
+    def draw_cells(self):
+        for cell in self.cells:
+            cell.display()
+            print(cell)
     
-    ### Draw Major Lines
-    for x in range(0, WINDOW_WIDTH, SQUARE_SIZE[0]): # draw vertical lines
-        pygame.draw.line(DISPLAYSURF, GRID_COLOR['Black'], (x,0),(x,WINDOW_HEIGHT))
-    for y in range (0, WINDOW_HEIGHT, SQUARE_SIZE[1]): # draw horizontal lines
-        pygame.draw.line(DISPLAYSURF, GRID_COLOR['Black'], (0,y), (WINDOW_WIDTH, y))
+    def get_clicked_cell(self, pos):
+        for cell in self.cells:
+            print(f'cell: {cell} pos: {pos}')
+            if cell.in_boundries(pos):
+                return cell
+
+        return None
+
+    def shuffle(self):
+        pass
 
 
-def initiateCells():
-    currentGrid = {}
-    fullCell = [1,2,3,4,5,6,7,8,9]
-    for xCoord in range(0,9):
-        for yCoord in range(0,9):
-            currentGrid[xCoord,yCoord] = list(fullCell) # Copies List
-    return currentGrid
+class Cell:
+    def __init__(self, coords, value, rect):
+        self.coords = coords
+        self.value = value
+        self.rect = rect
+    
+
+    def display(self):
+        cellSurf = BASICFONT.render('%s' %(self.value), True, GRID_COLOR['Black'])
+        x = self.rect.x + self.rect.width/2
+        y = self.rect.y + self.rect.height/2
+        cellRect = cellSurf.get_rect(center = (x, y))
+        #cellRect.topleft = (self.rect[0], self.rect[1])
+        DISPLAYSURF.blit(cellSurf, cellRect)
 
 
-def displayCells(grid):
-    # Create offset factors to display numbers in right location in cells.
-    xFactor = 0
-    yFactor = 0
-    for item in grid: # item is x,y co-ordinate from 0 - 8
-        cellData = grid[item] # isolates the numbers still available for that cell
-        for number in cellData: #iterates through each number
-            if number != ' ': # ignores those already dismissed
-                xFactor = ((number-1)%3) # 1/4/7 = 0 2/5/8 = 1 3/6/9 =2
-                if number <= 3:
-                    yFactor = 0
-                elif number <=6:
-                    yFactor = 1
-                else:
-                    yFactor = 2
-                #(item[0] * CELLSIZE) Positions in the right Cell
-                #(xFactor*NUMBERSIZE) Offsets to position number    
-                populateCells(number,(item[0]*CELL_SIZE[0])+(xFactor*NUMBER_SIZE[0]),(item[1]*CELL_SIZE[1])+(yFactor*NUMBER_SIZE[1]))
+    def in_boundries(self, pos):
+        x = self.rect.x
+        y = self.rect.y
+        z = x + self.rect.width
+        w = y + self.rect.height
+        if pos[0] >= x and pos[0] < z and pos[1] >= y and pos[1] < w:
+            return True
+
+        return False
+    
+    def __str__(self):
+        return f'Coords: {self.coords} -> Value: {self.value} Rect: {self.rect}'
 
 
-def populateCells(cellData, x, y):
-    cellSurf = BASICFONT.render('%s' %(cellData), True, GRID_COLOR[''])
-    cellRect = cellSurf.get_rect()
-    cellRect.topleft = (x, y)
-    DISPLAYSURF.blit(cellSurf, cellRect)
+
+class CellRect:
+    def __init__(self,x ,y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self) -> str:
+        return f'Rect x: {self.x} y: {self.y} width: {self.width} height: {self.height}'
+
+
+def create_cells():
+    cells = []
+    for x in range(1,10):
+        for y in range(1,10):
+            #cell_rect = [ (y-1)*CELL_SIZE[1] + CELL_SIZE[1]//2 , (x-1)*CELL_SIZE[0] + CELL_SIZE[0]//2 ]
+            i = (y-1)*CELL_SIZE[1]
+            j = (x-1)*CELL_SIZE[0]
+            width = CELL_SIZE[0]
+            height = CELL_SIZE[1]
+            cell_rect = CellRect(i,j,width,height)
+            cells.append(Cell([x,y], (x+y - 2)%9 + 1, cell_rect))
+    
+    return cells
 
 
 def main():
 
     global FPSCLOCK, DISPLAYSURF
     global BASICFONT, BASICFONTSIZE
-    BASICFONTSIZE = 15
+    BASICFONTSIZE = 25
     pygame.font.init()
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
-   
-
-   
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
     DISPLAYSURF.fill(BOARD_COLOR)
-    grid = initiateCells()
-    displayCells(grid)
-    draw_grid()
+
+    
+    grid = Grid(create_cells())
+    grid.draw_grid()
+    grid.draw_cells()
+
+    pygame.mouse.get_pos()
+
     pygame.display.set_caption("Sudoku")
     
-
     while True: #main game loop
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                selected_cell = grid.get_clicked_cell(pos)
+
+                if selected_cell is not None:
+                    print(selected_cell.value)
+                else:
+                    print(f"Didnt found a cell in {pos} boundries")
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
